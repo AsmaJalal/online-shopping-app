412adfcd1edf4a900c499d659cd4c156accf5657
Asma Arain <brdevteam@outlook.com>
2024-02-08 23:59:57 +0500

    ico added

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -14,7 +14,21 @@ const Home = () => {
           <div className="card-img-overlay d-flex align-items-center">
             <div className="container">
             <h4 className="card-title fs-1 text fw-lighter text-center">New Arrivals</h4>
-
+            <p className="lead text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+          facere doloremque veritatis odit similique sequi. Odit amet fuga nam
+          quam quasi facilis sed doloremque saepe sint perspiciatis explicabo
+          totam vero quas provident ipsam, veritatis nostrum velit quos
+          recusandae est mollitia esse fugit dolore laudantium. Ex vel explicabo
+          earum unde eligendi autem praesentium, doloremque distinctio nesciunt
+          porro tempore quis eaque labore voluptatibus ea necessitatibus
+          exercitationem tempora molestias. Ad consequuntur veniam sequi ullam
+          tempore vel tenetur soluta dolore sunt maxime aliquam corporis est,
+          quo saepe dolorem optio minus sint nemo totam dolorum! Reprehenderit
+          delectus expedita a alias nam recusandae illo debitis repellat libero,
+          quasi explicabo molestiae saepe, dolorem tempore itaque eveniet quam
+          dignissimos blanditiis excepturi harum numquam vel nihil? Ipsum
+        </p>
             </div>
           </div>
         </div>
